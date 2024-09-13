27806f63212ce32d4987274c00da1e7ae29c6bae
FuryPotat0 <oleg.bakharev.00@mail.ru>
2024-09-13 10:58:32 +0400

    Добавил реализацию первой ЛР

---
+++ company-service/src/main/java/com/ssau/companyservice/config/ServiceDescriptionConfig.java
@@ -0,0 +1,13 @@
+package com.ssau.companyservice.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class ServiceDescriptionConfig {
+    @Value("${custom.property.description}")
+    private String description;
+}
+
